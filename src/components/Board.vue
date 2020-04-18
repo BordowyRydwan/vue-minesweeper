fefccafe5b0ff00518366047dfc934e6725bce8f
BuildTools <unconfigured@null.spigotmc.org>
2020-04-18 21:40:04 +0200

    init

---
+++ src/components/Board.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Tile/>
+  </div>
+</template>
+
+<script>
+import Tile from './Tile.vue';
+
+export default {
+  name: 'Tile',
+  components: {
+    Tile
+  },
+  data(){
+    return{
+      boardWidth: 20,
+      boardHeight: 20,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
